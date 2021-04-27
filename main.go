b36a61b5981f6eef75df7e479a54296bc057676e
kmab <kmabadshah@gmaila.com>
2021-04-27 21:39:06 +0600

    implemented AddUser() and tested status code and body

--- main.go
+++ main.go
@@ -5,32 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-pg/pg/v10"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"reflect"
-	"runtime"
-
-	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"net/http"
 )
-
-type Client struct {
-	conn *websocket.Conn
-	name string
-}
-
-type Hub struct {
-	clients []Client
-	router  http.Handler
-}
-
-type User struct {
-	ID    uint   `json:"id"`
-	Uname string `json:"uname"`
-	Pass  string `json:"pass"`
-}
 
 var db = pg.Connect(&pg.Options{
 	Addr:     ":5432",
@@ -39,147 +17,55 @@ var db = pg.Connect(&pg.Options{
 	Database: "chat_test",
 })
 
-func init() {
-	ctx := context.Background()
-	if err := db.Ping(ctx); err != nil {
-		panic(err)
-	}
+type User struct {
+	Uname string `json:"uname"`
+	Pass  string `json:"pass"`
+	ID    int    `json:"id"`
 }
 
-func newHub() *Hub {
-	hub := Hub{}
-	upgrader := websocket.Upgrader{}
+func newRouter() *mux.Router {
 	router := mux.NewRouter()
-
-	router.PathPrefix("/users/{uname}").Methods("GET").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// get the user with {name}
-		uname := mux.Vars(r)["uname"]
-		// query the db
-		var user User
-		err := db.Model(&user).Where("uname=?", uname).Select()
-		if assertError(err) {
-			return
-		}
-
-		if user.ID == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte("invalid username"))
-			return
-		}
-
-		// encode and send user
-		encodedResBody, err := json.Marshal(user)
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(encodedResBody)
-	})
-	router.PathPrefix("/users").Methods("POST").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// get the data from req body and decode
+	router.Path("/users").Methods("POST").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		reqBody, err := ioutil.ReadAll(r.Body)
-		var decodedReqBody map[string]interface{}
-		err = json.Unmarshal(reqBody, &decodedReqBody)
-		isValidBody := decodedReqBody["uname"] != "" || decodedReqBody["pass"] != ""
-		if err != nil || !isValidBody {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = w.Write([]byte("invalid req body, must contain a uname and pass field"))
+		if assertRandomError(err, &w) {
 			return
 		}
 
-		// create the user
+		var decodedReqBody map[string]string
+		err = json.Unmarshal(reqBody, &decodedReqBody)
+		if err != nil || decodedReqBody["uname"] == "" || decodedReqBody["pass"] == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		user := User{
-			Uname: decodedReqBody["uname"].(string),
-			Pass:  decodedReqBody["pass"].(string),
+			Uname: decodedReqBody["uname"],
+			Pass:  decodedReqBody["pass"],
 		}
 		res, err := db.Model(&user).Insert()
-		if err != nil || res.RowsAffected() != 1 {
+		if err != nil || res.RowsAffected() != 1 || user.ID == 0 {
 			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Println("insertErr: ", err)
+			fmt.Println(err)
 			return
 		}
 
-		// encode and send the user
-		resBody, _ := json.Marshal(user)
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write(resBody)
-	})
-	router.PathPrefix("/connect/{name}").Methods("GET").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		name := mux.Vars(r)["name"]
-
-		// upgrade the req
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			prettyError(err)
-			return
-		}
-		defer conn.Close()
-
-		// add the client
-		clMain := Client{conn: conn, name: name}
-		hub.clients = append(hub.clients, clMain)
-
-		// notify the client
-		err = conn.WriteMessage(websocket.TextMessage, []byte("connection successful"))
-		if assertError(err) {
+		encodedResBody, err := json.Marshal(user)
+		if assertRandomError(err, &w) {
 			return
 		}
 
-		for {
-			_, msg, err := conn.ReadMessage()
-
-			if string(msg) != "" {
-				// decode
-				var decodedMsg map[string]string
-				err = json.Unmarshal(msg, &decodedMsg)
-				if err != nil {
-					prettyError(err)
-					return
-				}
-
-				// when msg recv from client1 send it to client2
-				receiver := decodedMsg["receiver"]
-				for _, client := range hub.clients {
-					if client.name == receiver {
-						err = client.conn.WriteMessage(websocket.TextMessage, []byte(decodedMsg["message"]))
-						if assertError(err) {
-							return
-						}
-					}
-				}
-			}
-
-			// remove the client on disconnect
-			if err != nil {
-				for i, client := range hub.clients {
-					if reflect.DeepEqual(client.conn, conn) {
-						hub.clients = append(hub.clients[:i], hub.clients[i+1:]...)
-					}
-				}
-
-				//prettyError(err)
-				break
-			}
+		_, err = w.Write(encodedResBody)
+		if assertRandomError(err, &w) {
+			return
 		}
-
 	})
 
-	hub.router = router
-
-	return &hub
+	return router
 }
 
-func prettyLog(v ...interface{}) {
-	_, fileName, line, _ := runtime.Caller(1)
-	fmt.Printf("[log] %s:%d\n", fileName, line)
-	spew.Dump(v...)
-}
-
-func prettyError(v ...interface{}) {
-	_, fileName, line, _ := runtime.Caller(1)
-	fmt.Printf("[error] %s:%d ", fileName, line)
-	fmt.Println(v...)
-}
-
-func assertError(err error) bool {
+func assertError(err error, w *http.ResponseWriter, statusCode int) bool {
 	if err != nil {
+		(*w).WriteHeader(statusCode)
 		fmt.Println(err)
 		return true
 	}
@@ -187,45 +73,13 @@ func assertError(err error) bool {
 	return false
 }
 
-func main() {
-	db := pg.Connect(&pg.Options{
-		Addr:     ":5432",
-		User:     "kmab",
-		Password: "kmab",
-		Database: "chat_test",
-	})
-	defer db.Close()
+func assertRandomError(err error, w *http.ResponseWriter) bool {
+	return assertError(err, w, http.StatusInternalServerError)
+}
 
+func init() {
 	ctx := context.Background()
 	if err := db.Ping(ctx); err != nil {
 		panic(err)
 	}
-
-	// delete all users
-	_, err := db.Model((*User)(nil)).Exec(`
-	    delete from users
-	`)
-	if err != nil {
-		panic(err)
-	}
-
-	// insert a user
-	user1 := &User{
-		Uname: "adnan",
-		Pass:  "badshah",
-	}
-	_, err = db.Model(user1).Insert()
-	if err != nil {
-		panic(err)
-	}
-
-	// get the user
-	var user User
-	db.Model(&user).Where(`id=?`, user1.ID).Select()
-
-	fmt.Println(user)
-}
-
-func init() {
-	log.SetFlags(0)
 }
--- main_test.go
+++ main_test.go
@@ -3,227 +3,56 @@ package chat
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"strings"
-	"sync"
 	"testing"
-	"time"
-
-	"github.com/gorilla/websocket"
 )
 
-var hub *Hub
-var testServer *httptest.Server
+func TestAddUser(t *testing.T) {
+	_, err := db.Exec(`delete from users`)
+	assertTestErr(t, err)
 
-func TestMain(m *testing.M) {
-	hub = newHub()
-	testServer = httptest.NewServer(hub.router)
-
-	code := m.Run()
-	testServer.Close()
-
-	os.Exit(code)
-}
-
-func TestOneClient(t *testing.T) {
-	url := "ws" + strings.TrimPrefix(testServer.URL, "http") + "/connect/client1"
-
-	t.Run("client added after successful connection", func(t *testing.T) {
-		conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-		if err != nil {
-			t.Fatal("dialErr: ", err)
-		}
-
-		got := len(hub.clients)
-		want := 1
-
-		if got != want {
-			t.Fatal("client not registered")
-		}
-
-		t.Run("client got notified about successful connection", func(t *testing.T) {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				t.Fatal("readMsg: ", err)
-			}
-			got := string(msg)
-			want := "connection successful"
-
-			if got != want {
-				t.Errorf("didn't get proper message, wanted %q, got %q", want, got)
-			}
-
-		})
-
-		t.Run("client removed after connection close", func(t *testing.T) {
-			err = conn.Close()
-			if err != nil {
-				t.Fatal("connCloseErr: ", err)
-			}
-
-			time.Sleep(time.Millisecond)
-			got = len(hub.clients)
-			want = 0
-
-			if got != want {
-				t.Errorf("client not removed")
-			}
-
-		})
-	})
-
-}
-
-func TestTwoClients(t *testing.T) {
-	// client 1
-	cl1 := createTestClient(t, testServer, "client1")
-	defer cl1.Close()
-
-	// client 2
-	cl2 := createTestClient(t, testServer, "client2")
-	defer cl2.Close()
-
-	testClientMsg(t, "client1msg -> client2", cl1, map[string]string{
-		"receiver": "client2",
-		"message":  "Hello client2",
-	}, cl2)
-
-	testClientMsg(t, "client2msg -> client1", cl2, map[string]string{
-		"receiver": "client1",
-		"message":  "Hello client1",
-	}, cl1)
-}
-
-func TestUsersEndpoint(t *testing.T) {
-	clearDB(t)
-	defer clearDB(t)
-
-	hub := newHub()
-	testServer := httptest.NewServer(hub.router)
+	router := newRouter()
+	testServer := httptest.NewServer(router)
 	defer testServer.Close()
 
+	url := testServer.URL + "/users"
 	reqBody := map[string]string{
 		"uname": "adnan",
 		"pass":  "badshah",
 	}
+	encodedReqBody, err := json.Marshal(reqBody)
+	assertTestErr(t, err)
 
-	t.Run("create a user", func(t *testing.T) {
-		url := testServer.URL + "/users/"
+	res, err := http.Post(url, "application/json", bytes.NewReader(encodedReqBody))
+	assertTestErr(t, err)
 
-		// POST the user
-		encodedReqBody, _ := json.Marshal(reqBody)
-		res, err := http.Post(url, "application/json", bytes.NewReader(encodedReqBody))
-		errSkip(t, err)
+	resBody, err := ioutil.ReadAll(res.Body)
+	assertTestErr(t, err)
 
-		// get the response
-		resBody, err := ioutil.ReadAll(res.Body)
-		errSkip(t, err)
+	var decodedResBody map[string]interface{}
+	err = json.Unmarshal(resBody, &decodedResBody)
+	assertTestErr(t, err)
 
-		var decodedResBody map[string]interface{}
-		err = json.Unmarshal(resBody, &decodedResBody)
-		errSkip(t, err)
+	t.Run("status code and body", func(t *testing.T) {
+		gotStatus := res.StatusCode
+		wantStatus := http.StatusOK
 
-		// check the response
-		if decodedResBody["uname"] != reqBody["uname"] || decodedResBody["pass"] != reqBody["pass"] {
-			t.Errorf("invalid res body, got %#v, wanted %#v", decodedResBody, reqBody)
-		}
-	})
-
-	t.Run("get a user", func(t *testing.T) {
-		url := testServer.URL + "/users/" + reqBody["uname"]
-
-		res, err := http.Get(url)
-		if err != nil {
-			t.Fatal(err)
+		if gotStatus != wantStatus {
+			t.Errorf("invalid code, wanted %#v, got %#v", wantStatus, gotStatus)
 		}
 
-		// decode
-		resBody, err := ioutil.ReadAll(res.Body)
-		errSkip(t, err)
-
-		var decodedResBody map[string]interface{}
-		err = json.Unmarshal(resBody, &decodedResBody)
-		errSkip(t, err)
-
-		if decodedResBody["uname"] != reqBody["uname"] || decodedResBody["pass"] != reqBody["pass"] {
-			t.Errorf("did not get the user created earlier")
-		}
-	})
-}
-
-func testClientMsg(t *testing.T, tname string, clSend *websocket.Conn, reqBody map[string]string, clRecv *websocket.Conn) {
-	clearDB(t)
-	defer clearDB(t)
-
-	t.Run(tname, func(t *testing.T) {
-		// read time limit
-		err := clRecv.SetReadDeadline(time.Now().Add(time.Millisecond))
-		errSkip(t, err)
-
-		var latestMsgRecv string
-		var wgRecv sync.WaitGroup
-		wgRecv.Add(1)
-
-		go func() {
-			for {
-				_, msg, err := clRecv.ReadMessage()
-				if err != nil {
-					break
-				}
-
-				latestMsgRecv = string(msg)
-			}
-
-			wgRecv.Done()
-		}()
-
-		for i := 0; i < 10; i++ {
-			// send message to clRecv from clSend
-			encodedReqBody, _ := json.Marshal(reqBody)
-			err = clSend.WriteMessage(websocket.TextMessage, encodedReqBody)
-			errSkip(t, err)
-
-			wgRecv.Wait()
-
-			//now clientRecv should get a message
-			got := latestMsgRecv
-			want := reqBody["message"]
-
-			if got != want {
-				t.Errorf("got %q but wanted %q", got, want)
+		for k, v := range reqBody {
+			if decodedResBody[k] != v {
+				t.Fatalf("invalid resp, got %#v, wanted %#v", decodedResBody, reqBody)
 			}
 		}
 	})
 }
 
-func createTestClient(t *testing.T, testServer *httptest.Server, cname string) *websocket.Conn {
-	url := "ws" + strings.TrimPrefix(testServer.URL, "http") + "/connect/" + cname
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		t.Fatal("dialError: ", err)
-	}
-
-	return conn
-}
-
-func errSkip(t *testing.T, err error) {
+func assertTestErr(t *testing.T, err error) {
 	t.Helper()
-	if err != nil {
-		fmt.Println(err)
-		t.Skip()
-	}
-}
-
-func clearDB(t *testing.T) {
-	t.Helper()
-	// clean the user table
-	_, err := db.Model((*User)(nil)).Exec(`
-		delete from users
-	`)
 	if err != nil {
 		t.Fatal(err)
 	}
